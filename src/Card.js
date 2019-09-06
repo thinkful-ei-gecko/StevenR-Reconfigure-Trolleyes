ba51b951338484cd639c702b8fb7cd25d5b5c036
stevenr1 <stevenr.thinkful@gmail.com>
2019-09-06 14:13:23 -0500

    new updates made to App.js and Card.js

--- src/Card.js
+++ src/Card.js
@@ -6,11 +6,14 @@ export default function Card(props) {
     <div className='Card'>
       <button
         type='button'
+        onClick={() => props.onClickDelete(props.id)}
       >
         delete
       </button>
-      <h3>{props.title}</h3>
-      <p>{props.content}</p>
-    </div>
-  )
-}
+      </div>
+
+      Card.propTypes = {
+        onClickDelete () => {}
+      }
+    
+  
